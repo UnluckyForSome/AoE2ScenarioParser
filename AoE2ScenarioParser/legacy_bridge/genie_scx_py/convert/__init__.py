a588c6308cec40f4297300a2eb1f8beb30ad94f4
UnluckyForSome <joemcclemont@hotmail.co.uk>
2026-05-10 17:13:39 +0100

    Track AOE2-McMinimap submodule at 60bdc65 (replace embedded tree)

--- AoE2ScenarioParser/legacy_bridge/genie_scx_py/convert/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from .mod import AutoToWK, ConvertError
-from .aoc_to_wk import AoCToWK
-from .hd_to_wk import HDToWK
-
-__all__ = ["ConvertError", "AutoToWK", "AoCToWK", "HDToWK"]
-
